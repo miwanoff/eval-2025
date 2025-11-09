e6ba61781366c9efc470f45239aad580291da7dc
miwanoff <marina.iva.noffa@gmail.com>
2025-11-09 13:42:12 +0200

    Add exception

--- eval.js
+++ eval.js
@@ -1,11 +1,18 @@
-let expr = "";
-let f;
-let x = prompt("Input x");
-expr = prompt("Input expression like x * x");
-let fstr = 'f = function (x) {return ' + expr + ';}'
-//console.log(fstr); 
-eval(fstr);
-alert(f(x));
+try {
+  let expr = "";
+  let f;
+  let x = prompt("Input x");
+  if (isNaN(x)){
+    throw new Error("Ви ввели не число!");    
+  }
+  expr = prompt("Input expression like x * x");
+  let fstr = "f = function (x) {return " + expr + ";}";
+  //console.log(fstr);
+  eval(fstr);
+  alert(f(x));
 
-let fF = new Function("x", "return " + expr);
-alert(fF(x));
+//   let fF = new Function("x", "return " + expr);
+//   alert(fF(x));
+} catch (ex) {
+  alert("Вибачте, виникла помилка: " + ex.message);
+}
